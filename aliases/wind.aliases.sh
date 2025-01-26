8839aa5d0a2ad43916e89e9babcd4d45f8642eab
Oscar Vuong <oscarvuong@gmail.com>
2025-01-26 07:28:06 -0800

    Changes to be committed: 	new file:   aliases/wind.aliases.sh 	new file:   configurations/zsh/.p10k.zsh 	modified:   configurations/zsh/zshrc 	modified:   paths/nvm/nvm.paths.sh

---
+++ aliases/wind.aliases.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+alias wind=windsurf
--- paths/nvm/nvm.paths.sh
+++ paths/nvm/nvm.paths.sh
@@ -6,18 +6,6 @@
 # License: MIT
 
 ## 🅽🆅🅼 🅿🅰🆃🅷
-if [[ "${OSTYPE}" == "darwin"* ]]; then
-  NVM_DIR="${HOME}"/.nvm
-  # shellcheck source=/dev/null
-  [[ -s "/opt/homebrew/opt/nvm/nvm.sh" ]] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
-  # shellcheck source=/dev/null
-  [[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
-elif [[ "${OSTYPE}" == "linux-gnu"* ]]; then
-  NVM_DIR="${HOME}"/.nvm
-  # shellcheck source=/dev/null
-  . "${NVM_DIR}/nvm.sh" # This loads nvm
-  # shellcheck source=/dev/null
-  . "${NVM_DIR}/bash_completion" # This loads nvm bash_completion
-fi
-export NVM_DIR
-export PATH="${NVM_DIR}:${PATH}"
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
